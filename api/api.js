69b3fdd0f81849ef905ea7f929264d1dce3a21a2
Esteban Madrigal <estebanmadrigal7@gmail.com>
2024-12-13 22:28:40 -0600

    Add logout route and jwt

--- api/api.js
+++ api/api.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import session from 'express-session';
-
-/* TODO: Check user in session (not being persistent) */
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 
 const users = [
     {
@@ -11,71 +10,53 @@ const users = [
     } 
 ];
 
+const SECRET_KEY = process.env.SECRET_KEY;
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
     origin: process.env.SERVER_BASE_URL,
     credentials: true,
 }));
 
-app.use(session({
-    secret: 'super-secret-key', 
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true, 
-        secure: false, 
-        sameSite: 'lax',
-        maxAge: 1000 * 60 * 60 
+const authenticateToken = (req, res, next) => {
+    const token = req.cookies.userId; 
+
+    if (!token) {
+        return res.status(401).json({ success: false, message: 'Session expired. Try to login!' });
     }
-}));
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(403).json({ success: false, message: 'Invalid token. You are not in session. Try to login' });
+        }
+        
+        req.user = user; 
+        next();
+    });
+};
 
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
 
     const user = users.find(u => u.username === username && u.password === password);   
 
-    if (user) {
-        console.log('Session init:', req.session); 
+    if (user) {   
+        const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1hr' }); 
 
-        req.session.user = { username }; 
-        
-        res.cookie('connect.sid', req.sessionID, {
-            httpOnly: true,  
-            secure: false,   
+        res.cookie('userId', token, {
+            httpOnly: true, 
+            secure: false,
             sameSite: 'lax', 
-            maxAge: 1000 * 60 * 60, 
+            maxAge: 3600000, 
         });
-        
-        console.log('Session after login:', req.session);      
+
         return res.status(200).json({ success: true, message: 'Login successful' });
     } else {
         return res.status(401).json({ success: false, message: 'Invalid username or password' });
-    }
-});
-
-app.post('/api/logout', (req, res) => {
-    console.log('Session before logout:', req.session);
-    if (req.session) {
-        req.session.destroy(err => {
-            if (err) {
-                return res.status(500).json({ success: false, message: 'Failed to logout from session' });
-            }
-
-            res.clearCookie('connect.sid', {
-                httpOnly: true,
-                secure: false,
-                sameSite: 'lax',
-            });
-
-            console.log('Session after logout:', req.session);
-
-            return res.status(200).json({ success: true, message: 'Logged out successfully' });
-        });
-    } else {
-        return res.status(400).json({ success: false, message: 'No active session to logout' });
     }
 });
 
@@ -93,12 +74,18 @@ app.post('/api/register', (req, res) => {
     return res.status(201).json({ success: true, message: 'User registered successfully' });
 });
 
-app.get('/api/check-session', (req, res) => {
-    if (req.session && req.session.user) {
-        return res.status(200).json({ isAuthenticated: true });
-    }
-    
-    return res.json({ isAuthenticated: false });
+app.post('/api/logout', authenticateToken, (req, res) => {
+    res.clearCookie('userId', {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'lax',
+    });
+
+    res.status(200).json({ success: true, message: 'User logged out successfuly'});
+});
+
+app.get('/api/checkUser', authenticateToken, (req, res) => {
+    res.status(200).json({ success: true, message: 'User already in session' });
 });
 
 const PORT = 8000;
