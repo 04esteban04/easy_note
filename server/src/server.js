5e77e906fef395f28103030b761e096065d8a4b9
Esteban Madrigal <estebanmadrigal7@gmail.com>
2025-01-15 16:49:19 -0600

    Feature: add create, edit, delete note endpoints

--- server/src/server.js
+++ server/src/server.js
@@ -11,6 +11,7 @@ app.use(cors({
 }));
 
 app.post('/auth/login', async (req, res) => {
+
     try {
         const response = await fetch(process.env.API_LOGIN_URL, {
             method: 'POST',
@@ -21,24 +22,28 @@ app.post('/auth/login', async (req, res) => {
             body: JSON.stringify(req.body),
         });
 
-        const data = await response.json();
-        
         const cookies = response.headers.get('set-cookie');
         res.setHeader('set-cookie', cookies); 
+        
+        const data = await response.json();
 
         if (data.success) {
             res.setHeader('set-cookie', cookies);
-            res.status(200).json({ success: true, message: data.message });
-        } else {
-            res.status(response.status).json(data);
-        }
-    } catch (error) {
+        } 
+        
+        res.json(data);
+
+    } 
+    
+    catch (error) {
         console.error('Login error in server:', error);
         res.status(500).json({ success: false, message: 'Internal server error (login)' });
     }
+
 });
 
 app.post('/auth/register', async (req, res) => {
+
     try {
         const response = await fetch(process.env.API_REGISTER_URL, {
             method: 'POST',
@@ -48,11 +53,14 @@ app.post('/auth/register', async (req, res) => {
         });
 
         const data = await response.json();
-        res.status(response.status).json(data);
-    } catch (error) {
+        res.json(data);
+    } 
+    
+    catch (error) {
         console.error('Register error in server:', error);
         res.status(500).json({ success: false, message: 'Internal server error (register)' });
     }
+
 });
 
 app.post('/auth/logout', async (req, res) => {
@@ -67,23 +75,23 @@ app.post('/auth/logout', async (req, res) => {
             credentials: 'include'
         });
         
-        const data = await response.json();
-        
         const cookies = response.headers.get('set-cookie');
         res.setHeader('set-cookie', cookies);  
+        
+        const data = await response.json();
+        res.json(data);
 
-        if (data.success) {
-            res.status(200).json({ success: true, message: data.message});
-        } else {
-            res.status(response.status).json(data);
-        }
-    } catch (error) {
+    } 
+    
+    catch (error) {
         console.error('Logout error in server:', error);
         res.status(500).json({ success: false, message: 'Internal server error (logout)' });
     }
+
 });
 
 app.get('/auth/checkUser', async (req, res) => {
+
     try {
         const response = await fetch(process.env.API_CHECK_USER, {
             method: 'GET',
@@ -96,10 +104,109 @@ app.get('/auth/checkUser', async (req, res) => {
 
         const data = await response.json();
         res.json(data);
-    } catch (error) {
-        console.error('CheckUser error in server:', error);
-        res.status(500).json({ success: false, message: 'Internal server error (checkUser)' });
+    } 
+    
+    catch (error) {
+        console.error('Check user error in server:', error);
+        res.status(500).json({ success: false, message: 'Internal server error (checking user)' });
     }
+
+});
+
+app.get('/getNotes', async (req, res) => {
+
+    try {
+        const response = await fetch(process.env.API_GET_NOTES, {
+            method: 'GET',
+            headers: { 
+                'Content-Type': 'application/json',
+                Cookie: req.headers.cookie || ''
+            },
+            credentials: 'include'
+        });
+
+        const data = await response.json();
+        res.json(data);
+    } 
+    
+    catch (error) {
+        console.error('Error while getting user notes in server:', error);
+        res.status(500).json({ success: false, message: 'Internal server error (getting notes)' });
+    }
+
+});
+
+app.post('/createNote', async (req, res) => {
+    
+    try {
+        const response = await fetch(process.env.API_CREATE_NOTE, {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                Cookie: req.headers.cookie || ''
+            },
+            credentials: 'include',
+            body: JSON.stringify(req.body)
+        });
+                
+        const data = await response.json();
+        res.json(data);
+
+    } 
+    
+    catch (error) {
+        console.error('Error while creating new note in server:', error);
+        res.status(500).json({ success: false, message: 'Internal server error (create note)' });
+    }
+
+});
+
+app.put('/updateNote', async (req, res) => {
+
+    try {
+        const response = await fetch(process.env.API_UPDATE_NOTE, {
+            method: 'PUT',
+            headers: { 
+                'Content-Type': 'application/json',
+                Cookie: req.headers.cookie || ''
+            },
+            credentials: 'include',
+            body: JSON.stringify(req.body)
+        });
+
+        const data = await response.json();      
+        res.json(data);
+    } 
+    
+    catch (error) {
+        console.error('Error while updating note in server:', error);
+        res.status(500).json({ success: false, message: 'Internal server error (updating note)' });
+    }
+
+});
+
+app.delete('/deleteNote', async (req, res) => {
+
+    try {
+        const response = await fetch(process.env.API_DELETE_NOTE, {
+            method: 'DELETE',
+            headers: { 
+                'Content-Type': 'application/json',
+                Cookie: req.headers.cookie || ''
+            },
+            credentials: 'include',
+            body: JSON.stringify(req.body)
+        });
+
+        const data = await response.json();
+        res.json(data);
+    } 
+    
+    catch (error) {
+        console.error('Error while deleting note in server:', error);
+        res.status(500).json({ success: false, message: 'Internal server error (deleting note)' });
+    }
+
 });
 
 const PORT = 5000;
